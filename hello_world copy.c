f16b0b7b2fab2a2a931c40fc879c28a4db06daec
aryanshygun <ryan.shygun@gmail.com>
2024-05-25 00:04:54 +0330

    commit

--- hello_world copy.c
+++ hello_world copy.c
@@ -1,14 +1,9 @@
-//
-//  main.c
-//  CLanguage
-//
-//  Created by Ryxn on 3/1/1403 AP.
-//
-
 #include <stdio.h>
-
 int main() {
-    printf("Hello, World!\n");
+    printf("    *    \n");
+    printf("   ***   \n");
+    printf("  *****  \n");
+    printf(" ******* \n");
+    printf("*********");
     return 0;
 }
-
